e8cf71e88c50b6b7618df1b3d7619b84b1f39b34
j-walleyLS <158578767+j-walleyLS@users.noreply.github.com>
2025-07-16 13:01:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,7 @@ st.markdown("""
     
     /* Consistent spacing for input fields */
     .stSidebar .stNumberInput {
-        margin-bottom: 1rem;
+        margin-bottom: 2rem;
     }
     .stSidebar .stSelectbox {
         margin-bottom: 0.4rem;
