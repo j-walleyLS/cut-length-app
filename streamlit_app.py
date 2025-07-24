483914b0429b3f7a13285abbf05aa51efeef01cd
j-walleyLS <158578767+j-walleyLS@users.noreply.github.com>
2025-07-24 10:32:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -346,8 +346,19 @@ if "unit_input_rows" not in st.session_state:
 # -----------------------------
 # Main UI
 # -----------------------------
-st.title("🪚 Cut Length Optimiser")
-st.markdown("*Optimize material cutting from various slab sizes*")
+# Create header with logo
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.title("🪚 Cut Length Optimiser")
+    st.markdown("*Optimize material cutting from various slab sizes*")
+with col2:
+    st.markdown("""
+    <div style="text-align: right; padding-top: 1rem;">
+        <img src="https://media.licdn.com/dms/image/v2/D4E0BAQHP523W42qnWw/company-logo_200_200/company-logo_200_200/0/1684478471407/london_stone_logo?e=2147483647&v=beta&t=dW-GcSLavPP7SD-qAqCyQgaH-LODRJqcoHyblQRHR9Q" 
+             style="height: 3rem; background: transparent;" 
+             alt="London Stone Logo"/>
+    </div>
+    """, unsafe_allow_html=True)
 
 # -----------------------------
 # Sidebar - Slab Selection
